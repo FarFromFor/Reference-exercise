8f53b8bc795394e4fdfba5bc30e8873c44630c81
Aleksandr Riabov <kridakasa@gmail.com>
2025-05-17 22:01:29 +0200

    Main functionality implemented

--- script.js
+++ script.js
@@ -9,21 +9,26 @@ class SearchPageLogic {
         this.allSportsBtn = document.getElementById('all_sports')
         this.allTypesBtn = document.getElementById('all_types')
 
-        this.allSportsAreSet = true
-        this.allTypesAreSet = true
+        this.resultsTitle = document.getElementById('results_title')
 
-        this.languageSelect = document.getElementById('language_select')
-        this.projectSelect = document.getElementById('project_select')
-        this.projectTypeSelect = document.getElementById('project_type_select')
+        this.loader = document.getElementById('loader')
 
-        this.sportsIDs = []
-        this.typesIDs = []
-
-        this.resources = []
         this.results = document.getElementById('results')
 
         this.modal = document.getElementById('modal')
         this.hideMudalBtn = document.getElementById('hide_btn')
+        this.alert = document.getElementById('alert')
+        this.reloadAlertBtn = document.getElementById('reload_alert_btn')
+        this.closeAlertBtn = document.getElementById('close_alert_btn')
+
+        this.allSportsAreSet = true
+        this.allTypesAreSet = true
+        this.languageSelect = document.getElementById('language_select')
+        this.projectSelect = document.getElementById('project_select')
+        this.projectTypeSelect = document.getElementById('project_type_select')
+        this.sportsIDs = []
+        this.typesIDs = []
+        this.resources = []
 
         this.handle()
     }
@@ -34,137 +39,11 @@ class SearchPageLogic {
         this.setAllTypes()
     }
 
-    showModal() {
-        this.modal.style.display = 'flex'
-    }
-
-    hideModal() {
-        console.log(this.modal)
-        this.modal.style.display = 'none'
-    }
-
-    showResults() {
-        const resultsTitle = document.createElement('h2')
-        resultsTitle.textContent = 'Výsledky'
-        this.results.parentNode.insertBefore(resultsTitle, this.results)
-        for (let i = 0; i < this.resources.length; i++) {
-            if (i === 0) {
-                this.results.appendChild(this.createSportResultsElement(i))
-                this.results.appendChild(this.createTypeResultsElement(i))
-            } else {
-                console.log(i)
-                if (this.resources[i - 1].sport.name !== this.resources[i].sport.name) {
-                    this.results.appendChild(this.createSportResultsElement(i))
-                }
-                if (this.resources[i - 1].type.name !== this.resources[i].type.name) {
-                    this.results.appendChild(this.createTypeResultsElement(i))
-                }
-            }
-            this.results.appendChild(this.createResourceElement(i))
-        }
-    }
-
-    clearResults() {
-        this.results.innerHTML = ''
-    }
-
-    createSportResultsElement(resourceIndex) {
-        const source = this.resources[resourceIndex]
-        const resultBlock = document.createElement('div');
-        resultBlock.classList.add('sport_results')
-        resultBlock.textContent = source.sport.name
-
-        return resultBlock
-    }
-
-    createTypeResultsElement(resourceIndex) {
-        const source = this.resources[resourceIndex]
-        const resultBlock = document.createElement('div');
-        resultBlock.classList.add('type_results')
-        resultBlock.textContent = source.type.name
-        return resultBlock
-    }
-
-    createResourceElement(resourceIndex) {
-        const source = this.resources[resourceIndex]
-        const resultBlock = document.createElement('div');
-        resultBlock.classList.add('result')
-        resultBlock.dataset.score = resourceIndex
-        const imagesSection = document.createElement('section');
-        imagesSection.classList.add('result_images')
-        source.images.forEach(i => {
-            const img = document.createElement('img');
-            img.classList.add('logo')
-            img.src = 'https://www.livesport.cz/res/image/data/' + i.path
-            img.alt = 'img'
-            imagesSection.appendChild(img)
-        })
-        if (source.images.length === 0) {
-            const img = document.createElement('img');
-            img.classList.add('logo')
-            img.src = './placeholder.png'
-            img.alt = 'img'
-            imagesSection.appendChild(img)
-        }
-        const infoSection = document.createElement('section');
-        const name = document.createElement('span');
-        name.textContent = source.name
-        infoSection.appendChild(name)
-        resultBlock.appendChild(imagesSection)
-        resultBlock.appendChild(infoSection)
-        resultBlock.addEventListener('click', ()=>{
-            this.showModal()
-        })
-        return resultBlock
-    }
-
-    async getResources() {
-        const link = this.createLink()
-        console.log(link)
-        try {
-            const response = await fetch(link, {
-                method: 'GET',
-                headers: {
-                    'Accept': 'application/json',
-                },
-            });
-            const data = await response.json();
-            this.resources = data
-            this.sortResources()
-            console.log(data)
-        } catch (error) {
-            console.log(error)
-        }
-    }
-
-    sortResources() {
-        this.resources.sort((a, b) => {
-            if (a.sport.id !== b.sport.id) {
-                return a.sport.id - b.sport.id;
-            } else {
-                return a.type.id - b.type.id;
-            }
-        });
-    }
-
-    createLink() {
-        const searchUrl = "https://s.livesport.services/api/v2/search"
-        const searchParams = new URLSearchParams({
-                'type-ids': this.typesIDs.join(','),
-                'project-type-id': this.projectTypeSelect[this.projectTypeSelect.selectedIndex].dataset.score,
-                'project-id': this.projectSelect[this.projectSelect.selectedIndex].dataset.score,
-                'lang-id': this.languageSelect[this.languageSelect.selectedIndex].dataset.score,
-                'q': this.input.value,
-                'sport-ids': this.sportsIDs.join(',')
-            }
-        )
-        return searchUrl + '?' + searchParams
-    }
-
     addEventListeners() {
         this.searchBtn.addEventListener('click', async () => {
-            await this.getResources()
             this.clearResults()
+            this.loader.style.display = 'block'
+            await this.getResources()
             this.showResults()
         })
 
@@ -196,6 +75,14 @@ class SearchPageLogic {
 
         this.hideMudalBtn.addEventListener('click', () => {
             this.hideModal()
+        })
+
+        this.closeAlertBtn.addEventListener('click', () => {
+            this.closeAlert()
+        })
+
+        this.reloadAlertBtn.addEventListener('click', () => {
+            this.reloadAlert()
         })
     }
 
@@ -264,6 +151,230 @@ class SearchPageLogic {
             }
         })
         this.allTypesAreSet = true
+    }
+
+    createLink() {
+        const searchUrl = "https://s.livesport.services/api/v2/search"
+        const searchParams = new URLSearchParams({
+                'type-ids': this.typesIDs.join(','),
+                'project-type-id': this.projectTypeSelect[this.projectTypeSelect.selectedIndex].dataset.score,
+                'project-id': this.projectSelect[this.projectSelect.selectedIndex].dataset.score,
+                'lang-id': this.languageSelect[this.languageSelect.selectedIndex].dataset.score,
+                'q': this.input.value,
+                'sport-ids': this.sportsIDs.join(',')
+            }
+        )
+        return searchUrl + '?' + searchParams
+    }
+
+    // Fetches resources from URL
+    async getResources() {
+        const link = this.createLink()
+        console.log(link)
+        try {
+            const response = await fetch(link, {
+                method: 'GET',
+                headers: {
+                    'Accept': 'application/json',
+                },
+            });
+            const data = await response.json();
+            this.resources = data
+            this.sortResources()
+            this.loader.style.display = 'none'
+            console.log(data)
+        } catch (error) {
+            this.loader.style.display = 'none'
+            this.alert.style.display = 'block'
+        }
+    }
+
+    // Sort received results before printing
+    sortResources() {
+        this.resources.sort((a, b) => {
+            if (a.sport.id !== b.sport.id) {
+                return a.sport.id - b.sport.id;
+            } else {
+                return a.type.id - b.type.id;
+            }
+        });
+    }
+
+    createSportResultsElement(resourceIndex) {
+        const source = this.resources[resourceIndex]
+        const resultBlock = document.createElement('div');
+        resultBlock.classList.add('sport_results')
+        resultBlock.textContent = this.translate(source.sport.name)
+
+        return resultBlock
+    }
+
+    createTypeResultsElement(resourceIndex) {
+        const source = this.resources[resourceIndex]
+        const resultBlock = document.createElement('div');
+        resultBlock.classList.add('type_results')
+        resultBlock.textContent = this.translate(source.type.name)
+        return resultBlock
+    }
+
+    createResourceElement(resourceIndex) {
+        const source = this.resources[resourceIndex]
+        const resultBlock = document.createElement('div');
+        resultBlock.classList.add('result')
+        resultBlock.dataset.score = resourceIndex
+        const imagesSection = document.createElement('section');
+        imagesSection.classList.add('result_images')
+        source.images.forEach(i => {
+            const img = document.createElement('img');
+            img.classList.add('logo')
+            img.src = 'https://www.livesport.cz/res/image/data/' + i.path
+            img.alt = 'img'
+            imagesSection.appendChild(img)
+        })
+        if (source.images.length === 0) {
+            const img = document.createElement('img');
+            img.classList.add('logo')
+            img.src = './placeholder.png'
+            img.alt = 'img'
+            imagesSection.appendChild(img)
+        }
+        const infoSection = document.createElement('section');
+        const name = document.createElement('span');
+        name.textContent = source.name
+        infoSection.appendChild(name)
+        resultBlock.appendChild(imagesSection)
+        resultBlock.appendChild(infoSection)
+        resultBlock.addEventListener('click', () => {
+            this.showModal(resourceIndex)
+        })
+        return resultBlock
+    }
+
+    // Dummy function for translation
+    translate(message) {
+        const translations = {
+            'Soccer': 'Fotbal',
+            'Tennis': 'Tenis',
+            'Basketball': 'Basketbal',
+            'Hockey': 'Hokej',
+            'American football': 'Americký footbal',
+            'Baseball': 'Basebal',
+            'Rugby Union': 'Rugby',
+            'Handball': 'Házená',
+            'Floorball': 'Florbal',
+            'Team': 'Tým',
+            'TournamentTemplate': 'Soutěže',
+            'Player': 'Hráči jednotlivci',
+            'PlayerInTeam': 'Hráči v týmech',
+        }
+        return translations[message] ? translations[message] : message
+    }
+
+    // Print received resources
+    showResults() {
+        if (this.resources.length > 0) {
+            this.resultsTitle.style.display = 'block'
+        } else {
+            this.resultsTitle.style.display = 'none'
+        }
+        for (let i = 0; i < this.resources.length; i++) {
+            if (i === 0) {
+                this.results.appendChild(this.createSportResultsElement(i))
+                this.results.appendChild(this.createTypeResultsElement(i))
+            } else {
+                console.log(i)
+                if (this.resources[i - 1].sport.name !== this.resources[i].sport.name) {
+                    this.results.appendChild(this.createSportResultsElement(i))
+                }
+                if (this.resources[i - 1].type.name !== this.resources[i].type.name) {
+                    this.results.appendChild(this.createTypeResultsElement(i))
+                }
+            }
+            this.results.appendChild(this.createResourceElement(i))
+        }
+    }
+
+    clearResults() {
+        this.results.innerHTML = ''
+    }
+
+    showModal(resourceIndex) {
+        this.fillModalContent(resourceIndex)
+        this.modal.style.display = 'flex'
+    }
+
+    hideModal() {
+        console.log(this.modal)
+        this.modal.style.display = 'none'
+    }
+
+    // Reset modal content with received resource with id = resourceIndex
+    fillModalContent(resourceIndex) {
+        const photosContainer = this.modal.querySelector(`[id="profile_photos"]`)
+        photosContainer.innerHTML = ''
+        this.resources[resourceIndex].images.forEach(i => {
+            const img = document.createElement('img');
+            img.classList.add('profile_image')
+            console.log(this.resources[resourceIndex])
+            img.src = 'https://www.livesport.cz/res/image/data/' + i.path
+            img.alt = 'img'
+            photosContainer.appendChild(img)
+        })
+
+        if (this.resources[resourceIndex].images.length === 0) {
+            const img = document.createElement('img');
+            img.classList.add('profile_image')
+            img.src = './placeholder.png'
+            img.alt = 'img'
+            photosContainer.appendChild(img)
+        }
+
+        const profileInfo = this.modal.querySelector(`[id="profile_specific_info"]`)
+        profileInfo.innerHTML = ''
+
+        if (this.resources[resourceIndex].type.id < 3) {
+            if (this.resources[resourceIndex].name) {
+                const nameSection = document.createElement('section');
+                nameSection.classList.add('specific_info')
+                nameSection.textContent = 'Název týmu: ' + this.resources[resourceIndex].name
+                profileInfo.appendChild(nameSection)
+            }
+        } else {
+            if (this.resources[resourceIndex].name) {
+                let nameSection = document.createElement('section');
+                nameSection.classList.add('specific_info')
+                nameSection.textContent = 'Jméno: ' + this.resources[resourceIndex].name
+                profileInfo.appendChild(nameSection)
+            }
+            if (this.resources[resourceIndex].gender) {
+                let sexSection = document.createElement('section');
+                sexSection.classList.add('specific_info')
+                sexSection.textContent = 'Pohlaví: ' + this.resources[resourceIndex].gender.name
+                profileInfo.appendChild(sexSection)
+            }
+        }
+
+        if (this.resources[resourceIndex].defaultCountry) {
+            const section = document.createElement('section');
+            section.classList.add('specific_info')
+            section.textContent = 'Stát: ' + this.resources[resourceIndex].defaultCountry.name
+            profileInfo.appendChild(section)
+        }
+        if (this.resources[resourceIndex].url) {
+            let urlSection = document.createElement('section');
+            urlSection.classList.add('specific_info')
+            urlSection.textContent = 'URL: ' + this.resources[resourceIndex].url
+            profileInfo.appendChild(urlSection)
+        }
+    }
+
+    closeAlert() {
+        this.alert.style.display = 'none'
+    }
+
+    reloadAlert() {
+        this.closeAlert()
+        this.searchBtn.click()
     }
 }
 
